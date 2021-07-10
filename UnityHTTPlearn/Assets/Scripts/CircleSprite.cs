29eb2e62f7826090b12837bfd2aad68ca3728dad
takashi0nakamura <tnakamura@ic.kanagawa-it.ac.jp>
2021-07-10 21:52:38 +0900

    とりあえず丸いスプライトがくるくる回るだけのコードを作成した。

---
+++ UnityHTTPlearn/Assets/Scripts/CircleSprite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSprite : MonoBehaviour
+{
+    private float elapsedTime; // オブジェクトが生成されてからの経過時間(sec)
+    static float period = 0.5f; // アニメーションの周期 (sec)
+    static float maxAmplitude = 3.0f; // 振幅 
+
+    private float radius; // 半径
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0.0f;
+        radius = maxAmplitude; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 経過時間を計算する
+        elapsedTime += Time.deltaTime;
+
+        // 現在の角度をラジアンで求める
+        float rad = elapsedTime / period * 2 * Mathf.PI; 
+
+        Vector3 cpos = new Vector3( Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 1.0f );
+        transform.position = cpos;
+
+    }
+}
