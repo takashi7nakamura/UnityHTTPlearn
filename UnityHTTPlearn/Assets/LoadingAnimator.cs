962465fe8d7aec6c5f6e0e82b5de530da7185eea
takashi0nakamura <tnakamura@ic.kanagawa-it.ac.jp>
2021-07-10 20:01:38 +0900

    最初のコミット, Coroutine の動作テストを追加 .gitignore が追加になっていなかったので追加。

---
+++ UnityHTTPlearn/Assets/LoadingAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コルーチンを実行
+        StartCoroutine("LoadingAnimationSequence");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadingAnimationSequence()
+    {
+        // コルーチンで実行される処理
+        Debug.Log("Start Coroutine");
+
+        yield return new WaitForSeconds(3);
+
+        Debug.Log("Done");
+    }
+}
